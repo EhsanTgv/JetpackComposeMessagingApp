e5c51d4ab773a0146f2941ad94aae4b7b534f9a1
EhsanTgv <EhsanTgv@gmail.com>
2021-03-05 00:11:42 +0330

    add actions & color to TitleBar

--- app/src/main/java/com/example/jetpackcomposemessagingapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposemessagingapp/MainActivity.kt
@@ -3,11 +3,23 @@ package com.example.jetpackcomposemessagingapp
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.animatedVectorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposemessagingapp.ui.theme.JetpackComposeMessagingAppTheme
 
 class MainActivity : AppCompatActivity() {
@@ -17,11 +29,41 @@ class MainActivity : AppCompatActivity() {
             JetpackComposeMessagingAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    messageListTopBar("Android")
                 }
             }
         }
     }
+}
+
+@Composable
+fun messageListTopBar(title: String) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                style = TextStyle(
+                    textAlign = TextAlign.Center,
+                    fontStyle = FontStyle.Normal,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            )
+        },
+        actions = {
+            Image(
+                painter = painterResource(id =  R.drawable.ic_search),
+                contentDescription = "",
+                modifier = Modifier.padding(start = 10.dp, end = 8.dp)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.ic_options_menu),
+                contentDescription = "",
+                modifier = Modifier.padding(start = 10.dp, end = 8.dp)
+            )
+        },
+        backgroundColor = Color.White
+    )
 }
 
 @Composable
@@ -33,6 +75,9 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JetpackComposeMessagingAppTheme {
-        Greeting("Android")
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+            messageListTopBar("Android")
+        }
     }
 }
