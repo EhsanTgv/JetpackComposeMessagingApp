9f97f8213827b170411bfaaab86cc7a5c4035932
EhsanTgv <EhsanTgv@gmail.com>
2021-03-05 01:08:41 +0330

    Implement Sender & SenderIcon

--- app/src/main/java/com/example/jetpackcomposemessagingapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposemessagingapp/MainActivity.kt
@@ -4,19 +4,22 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.animatedVectorResource
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,6 +40,15 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeMessagingAppTheme {
+        // A surface container using the 'background' color from the theme
+        SenderIcon()
+    }
+}
+
 @Composable
 fun MessagesListScreenView(
     title: String,
@@ -46,17 +58,6 @@ fun MessagesListScreenView(
     Scaffold(
         topBar = { MessageListTopBar(title = title) },
         floatingActionButton = { FloatingButton(fabClick = fabClick) }) {
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposeMessagingAppTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(color = MaterialTheme.colors.background) {
-            MessagesListScreenView("Messages", {}, {})
-        }
     }
 }
 
@@ -95,5 +96,28 @@ fun FloatingButton(fabClick: () -> Unit) {
     FloatingActionButton(onClick = { fabClick() }) {
         Icon(painter = painterResource(id = R.drawable.ic_add), contentDescription = "")
     }
+}
+
+@Composable
+fun SenderIcon() {
+    Box(
+        modifier = Modifier
+            .clip(CircleShape)
+            .size(30.dp)
+            .background(Color.DarkGray)
+    ) {
+        Icon(painter = painterResource(id = R.drawable.ic_person), contentDescription = "")
+    }
+}
+
+@Composable
+fun Sender(sender: String, modifier: Modifier = Modifier) {
+    Text(
+        text = sender,
+        style = TextStyle(color = Color.Black, fontSize = 18.sp),
+        modifier = modifier,
+        overflow = TextOverflow.Ellipsis,
+        maxLines = 1
+    )
 }
 
